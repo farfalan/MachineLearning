60cf1fc3a4a89ff36fffb505d58fcc4a2cb55fb1
root <root@DESKTOP-JHF76N5.>
2025-01-24 17:07:53 +0100

    Boston Housing

--- ISPL.py
+++ ISPL.py
@@ -1,5 +1,9 @@
 from ISLP import load_data
 import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+import seaborn as sns
 import numpy as np
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
@@ -9,17 +13,38 @@ from ISLP.models import (ModelSpec as MS,
 
 Boston = load_data('Boston')
 
-X= Boston['lstat']
-X = sm.add_constant(X)
-y = Boston['medv']
-model = sm.OLS(y, X)
-results = model.fit()
+df = pd.DataFrame(Boston)
+X = df.iloc[:, 0:12]
+y = df.iloc[:,12]
 
+X_test, X_train, y_test, y_train = train_test_split(X, y, test_size=0.2, random_state=42)  
 
-ax = Boston.plot.scatter('lstat', 'medv')
+model = LinearRegression()
+model.fit(X_train,y_train)
 
-xlim = ax.get_xlim ()
-ylim = [results.params[0] + results.params[1] * x for x in xlim]
+y_pred = model.predict(X_test)
 
-plt.plot(xlim, ylim, color='red')
+acuracy = model.score(X_test,y_test)
+
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+
+print(f"Mean Squared Error: {mse}")
+print(f"R-squared: {r2}")
+
+# Residual Plot
+plt.figure(figsize=(10, 5))
+sns.residplot(x=y_pred, y=y_test - y_pred, lowess=True)
+plt.xlabel('Predicted values')
+plt.ylabel('Residuals')
+plt.title('Residual Plot')
+plt.show()
+
+# Actual vs. Predicted Plot
+plt.figure(figsize=(10, 5))
+plt.scatter(y_test, y_pred, alpha=0.5)
+plt.plot([min(y_test), max(y_test)], [min(y_test), max(y_test)], color='red', linewidth=2)
+plt.xlabel('Actual values')
+plt.ylabel('Predicted values')
+plt.title('Actual vs. Predicted')
 plt.show()
